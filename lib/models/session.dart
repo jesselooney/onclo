8060c7a665ed5cc927ef7172ee8e9c86be495c40
Jesse Looney <42242696+jesselooney@users.noreply.github.com>
2025-08-17 15:47:28 -0400

    document session.dart

--- lib/models/session.dart
+++ lib/models/session.dart
@@ -1,13 +1,25 @@
 import 'package:onclo/database.dart';
 import 'package:onclo/models/activity.dart';
 
+/// A contiguous period of time spent doing a certain [Activity].
+///
+/// Each session is backed by a [SessionEnd], a representation more suitable
+/// for UI logic and persistence. Given a session end the date at which the
+/// represented session started, one can construct the full session using
+/// [Session.fromSessionEnd].
 class Session {
+  /// The `id` of the [SessionEnd] backing this session.
   final int sessionEndId;
+  /// The moment in time marking the start of this session.
   final DateTime startDate;
+  /// The moment in time marking the end of this session.
   final DateTime endDate;
+  /// The [Activity] performed during this session.
   final Activity activity;
+  /// An arbitrary note specific to this session.
   final String note;
 
+  /// How long the session lasted, based on its start and end dates.
   Duration get duration => endDate.difference(startDate);
 
   const Session({
@@ -18,6 +30,10 @@ class Session {
     required this.note,
   });
 
+  /// Constructs a session from a [SessionEnd] and the session's start date.
+  ///
+  /// The resulting session's fields match those of the session end except for
+  /// its start date, which is just `startDate`.
   Session.fromSessionEnd(SessionEnd sessionEnd, DateTime startDate)
     : this(
         sessionEndId: sessionEnd.id,
